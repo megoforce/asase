d909042eecfea520bab04de8cd909bc4a5ec2939
Julio Quiroz <chulini@gmail.com>
2016-04-08 20:13:59 -0300

    shoal

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -4,26 +4,42 @@ using System.Collections;
 public class Fish : MonoBehaviour {
 	[SerializeField]
 	Issue issue;
-
+	TrailRenderer myTrailRenderer;
+	Transform myTransform;
+	float speed = 0.3f;
+	float amplitude = 15f;
+	float seed;
 	public static Material solvedMaterial, unsolvedMaterial;
 	void Awake(){
-		if(solvedMaterial == null){
-			solvedMaterial = Resources.Load("Materials/Solved") as Material;
-			unsolvedMaterial = Resources.Load("Materials/Unsolved") as Material;
+		myTransform = GetComponent<Transform>();
+		myTrailRenderer = GetComponent<TrailRenderer>();
+
+		if(Fish.solvedMaterial == null){
+			Fish.solvedMaterial = Resources.Load("Materials/Solved") as Material;
+			Fish.unsolvedMaterial = Resources.Load("Materials/Unsolved") as Material;
 		}
+
+		seed = Random.Range(0,1234567f);
 	}
 
 	public void Initialize(Issue _issue,double minUnsolved, double maxUnsolved){
 		issue = _issue;
-		GetComponent<Renderer>().sharedMaterial = (issue.solved) ? solvedMaterial : unsolvedMaterial;
+		myTrailRenderer.sharedMaterial = (issue.solved) ? solvedMaterial : unsolvedMaterial;
+
 		if(!issue.solved){
 			double delta = maxUnsolved - minUnsolved;
-			GetComponent<Transform>().localScale = (float)(0.5f + ((issue.unsolvedMinutes-minUnsolved) / delta))*Vector3.one;
+			myTrailRenderer.startWidth = (float)(0.1f + ((issue.unsolvedMinutes-minUnsolved) / delta));
 		} else {
-			GetComponent<Transform>().localScale = 0.5f*Vector3.one;
+			myTrailRenderer.startWidth = 0.1f;
 		}
-		
-		
-			
+	}
+
+	void Update(){
+		float xPosition = (-amplitude/2f + Mathf.PerlinNoise(seed,Time.time*speed)*amplitude)*(16f/9f);
+		float yPosition = -amplitude/2f + Mathf.PerlinNoise(Time.time*speed,seed)*amplitude;
+
+
+		myTransform.position = new Vector3(xPosition,yPosition,0);
+
 	}
 }
